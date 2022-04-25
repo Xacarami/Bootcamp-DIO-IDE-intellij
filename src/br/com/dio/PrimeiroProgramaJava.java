fc298cd5d11230cb2629889f14a4ace686391642
Xacarami <gabriel.plautz2018@gmail.com>
2022-04-25 16:30:53 -0300

    Class livri pelo intellij

--- src/br/com/dio/PrimeiroProgramaJava.java
+++ src/br/com/dio/PrimeiroProgramaJava.java
@@ -9,9 +9,46 @@ public class PrimeiroProgramaJava {
 
         System.out.println(gato);
 
+        Livro livro = new Livro("O mágico de OZ", 260);
+
+        System.out.println(livro);
 
         /*int a = 5;
         int b = 2;
         System.out.println("Até a Lua " + (a+b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
     }
 }
